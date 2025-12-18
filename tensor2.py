c3734fb4f6c6c112a1fe04f894cb86b95c00d2ba
fajartd02 <fajarmh2@gmail.com>
2025-12-19 03:18:28 +0900

    tensor 1 and 2

--- tensor2.py
+++ tensor2.py
@@ -7,3 +7,12 @@ X1 = torch.tensor([10, 38, 100, 150])
 
 y_pred = 1 * b + X1 * w1
 print(y_pred)
+
+
+print(b.shape)
+print(X1.shape)
+print(b.size())
+print(X1.size())
+
+print(y_pred[1])
+print(y_pred[1].item())
